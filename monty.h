3ed46d0524f6f4929005caf916992a37bb40e030
WisDomz <houseofwem1@gmail.com>
2023-10-18 12:21:59 +0100

    This is the header file

--- monty.h
+++ monty.h
@@ -32,5 +32,8 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void push(stack_t **stack, int n);
+void pall(stack_t **stack, unsigned int line_number);
+
 
 #endif
