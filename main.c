99eabde2b0150c7ea768143749225b199ac37d4a
WisDomz <houseofwem1@gmail.com>
2023-10-18 12:20:18 +0100

    This is the main file

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+/**
+ * main - is the main fuction
+ * Return: will return 0 which means positive
+ */
+int main(void)
+{
+	stack_t *stack = NULL;
+
+	push(&stack, 1);
+	push(&stack, 2);
+	push(&stack, 3);
+
+	pall(&stack, 0);
+
+	return (0);
+}
