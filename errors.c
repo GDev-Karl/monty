91b0e5d5dd1222ed1f792a7ca2d44b9ea51ef27f
Karl <tekamkarl@gmail.com>
2023-10-20 13:59:11 +0200

    second modification

--- errors.c
+++ errors.c
@@ -11,33 +11,51 @@ int error_with_usage(void)
 	return (EXIT_FAILURE);
 }
 
+/**
+ * failed_open - cannot open a file
+ * @file_name: the name of the file
+ *
+ * Return: EXIT_FAILURE
+ */
 int failed_open(char *file_name)
 {
-    fprintf(stderr, "Can't open file %s\n", file_name);
-    return (EXIT_FAILURE);
+	fprintf(stderr, "Can't open file %s\n", file_name);
+	return (EXIT_FAILURE);
 }
 
 /**
- * malloc_error - Prints malloc error messages.
+ * allocation_error - malloc failed
  *
  * Return: (EXIT_FAILURE) always.
  */
-int malloc_error(void)
+int allocation_error(void)
 {
 	fprintf(stderr, "Error: malloc failed\n");
 	return (EXIT_FAILURE);
 }
 
 /**
- * unknown_op_error - Prints unknown instruction error messages.
+ * unknown_instruction - Prints unknown instruction where
  * @opcode: Opcode where error occurred.
- * @line_number: Line number in Monty bytecodes file where error occured.
+ * @line_number: Line number in Monty file
  *
- * Return: (EXIT_FAILURE) always.
+ * Return: EXIT_FAILURE
  */
-int unknown_op_error(char *opcode, unsigned int line_number)
+int unknown_instruction(char *opcode, unsigned int line_number)
 {
 	fprintf(stderr, "L%u: unknown instruction %s\n",
 		line_number, opcode);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * type_error - Prints invalid type. must be an integer
+ * @line_number: Line number in Monty bytecodes file where error occurred.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int type_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%u: usage: push integer\n", line_number);
 	return (EXIT_FAILURE);
 }
--- main.c
+++ main.c
@@ -36,7 +36,7 @@ int main(void)
 	fclose(fp);
 
 	return (0);
-} 
+}
 */
 
 #include "monty.h"
@@ -44,7 +44,7 @@ int main(void)
 #include <sys/stat.h>
 #include <fcntl.h>
 
-char **op_toks = NULL;
+char **op_tokens = NULL;
 
 /**
  * main - the entry point for Monty Interp
